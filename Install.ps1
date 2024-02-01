961cb612ce868da411b911522df612d4ec0bb706
Taha <taharaed98@gmail.com>
2024-02-01 20:38:08 +0100

    Uncomment code

--- Install.ps1
+++ Install.ps1
@@ -8,14 +8,14 @@ $private:stuffDirectory = Join-Path $modulesPath "Stuff"
 New-Item -Path $stuffDirectory -ItemType Directory -Force -ErrorAction SilentlyContinue
 Copy-Item "$PSScriptRoot/Stuff.psm1" $stuffDirectory
 
-# Set-ExecutionPolicy Bypass -Scope Process -Force 
-# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# # Default is wsl2 with Ubuntu
-# wsl --install
+# Default is wsl2 with Ubuntu
+wsl --install
 
-# $packagesConfig = "$PSScriptRoot\packages.config"
+$packagesConfig = "$PSScriptRoot\packages.config"
 
-# # Exit code 3010 means "requires reboot"
-# choco install $packagesConfig -y --ignore-package-exit-codes=3010 --timeout=0
+# Exit code 3010 means "requires reboot"
+choco install $packagesConfig -y --ignore-package-exit-codes=3010 --timeout=0
